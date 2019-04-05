567f40265f233e2acf4984f50940bbaf82ab7bbd
Robin Buckley <robin.buckley@auspost.com.au>
2019-04-05 13:33:16 +1100

    clear up packages

--- cli.js
+++ cli.js
@@ -2,6 +2,7 @@
 const chalk = require('chalk');
 const readline = require('readline');
 const Choices = require('prompt-choices');
+var inquirer = require('inquirer');
 
 console.log(chalk.blue('////////////////////////////////////'));
 console.log(chalk.blue('// AMU') + ' Project' + chalk.red(' Scaffolding'));
@@ -29,4 +30,54 @@ var choices = new Choices(['foo', 'bar', 'baz', 'qux', 'fez'], {
 choices.get('foo').disabled = true;
 // console.log(choices.get('foo'))
 // console.log(choices.get('bar'))
-console.log(choices.render(1))
+// console.log(choices.render(1))
+
+let questions = [{
+  type: 'expand',
+  name: 'toppings',
+  message: 'What about the toppings?',
+  choices: [
+    {
+      key: 'p',
+      name: 'Pepperoni and cheese',
+      value: 'PepperoniCheese'
+    },
+    {
+      key: 'a',
+      name: 'All dressed',
+      value: 'alldressed'
+    },
+    {
+      key: 'w',
+      name: 'Hawaiian',
+      value: 'hawaiian'
+    }
+  ]
+}, {
+  type: 'expand',
+  name: 'toppings',
+  message: 'What about the toppings?',
+  choices: [
+    {
+      key: 'p',
+      name: 'Pepperoni and cheese',
+      value: 'PepperoniCheese'
+    },
+    {
+      key: 'a',
+      name: 'All dressed',
+      value: 'alldressed'
+    },
+    {
+      key: 'w',
+      name: 'Hawaiian',
+      value: 'hawaiian'
+    }
+  ]
+}];
+
+inquirer.prompt(questions).then(answers => {
+  console.log('\nOrder receipt:');
+  console.log(JSON.stringify(answers, null, '  '));
+});
+
